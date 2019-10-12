8d782b1036341533070b459c7bb13c2a81cb0ee5
Zach Shipko <zachshipko@gmail.com>
2019-10-11 18:05:38 -0700

    init

---
+++ src/compile.rs
@@ -0,0 +1,141 @@
+use std::env;
+use std::fs::remove_file;
+use std::io;
+use std::path::PathBuf;
+use std::process::Command;
+
+pub struct Build<'a> {
+    pub halide_path: PathBuf,
+    pub src: Vec<PathBuf>,
+    pub output: PathBuf,
+    pub cxx: Option<&'a str>,
+    pub cxxflags: Option<&'a str>,
+    pub ldflags: Option<&'a str>,
+    pub build_args: Vec<&'a str>,
+    pub run_args: Vec<&'a str>,
+    pub keep: bool,
+    pub generator: bool,
+}
+
+impl<'a> Build<'a> {
+    pub fn build(&self) -> io::Result<bool> {
+        let cxx_default = env::var("CXX").unwrap_or("c++".to_string());
+        let mut cmd = Command::new(self.cxx.clone().unwrap_or(cxx_default.as_str()));
+
+        cmd.args(&["-I", &self.halide_path.join("include").to_string_lossy()])
+            .args(&["-I", &self.halide_path.join("tools").to_string_lossy()]);
+
+        if let Some(flags) = &self.cxxflags {
+            cmd.args(flags.split(" "));
+        }
+
+        if self.generator {
+            cmd.arg(
+                &self
+                    .halide_path
+                    .join("tools")
+                    .join("GenGen.cpp")
+                    .to_string_lossy()
+                    .as_ref(),
+            );
+            cmd.args(&self.build_args);
+        }
+
+        cmd.args(&self.src)
+            .args(&["-o", &self.output.to_string_lossy()])
+            .args(&[
+                "-L",
+                &self.halide_path.join("lib").to_string_lossy(),
+                "-lHalide",
+                "-lpng",
+                "-ljpeg",
+                "-lpthread",
+                "-ltinfo",
+                "-ldl",
+                "-lz",
+            ]);
+
+        if let Some(flags) = &self.ldflags {
+            cmd.args(flags.split(" "));
+        }
+
+        cmd.status().map(|status| status.success())
+    }
+
+    pub fn run(&self) -> io::Result<bool> {
+        if !self.output.exists() {
+            return Ok(false);
+        }
+
+        let res = Command::new(&self.output)
+            .args(&self.run_args)
+            .env("LD_LIBRARY_PATH", self.halide_path.join("lib"))
+            .status()
+            .map(|status| status.success());
+        if !self.keep {
+            remove_file(&self.output)?;
+        }
+        res
+    }
+}
+
+/*pub fn run<P: AsRef<Path>, Q: AsRef<Path>>(
+    halide_path: P,
+    path: Q,
+    run_args: Vec<&str>,
+    generator: bool,
+) -> io::Result<bool> {
+    let build = Build {
+        halide_path: halide_path.as_ref().to_path_buf(),
+        src: vec![path.as_ref().to_path_buf()],
+        output: path.as_ref().to_path_buf().with_extension("exe"),
+        cxx: None,
+        cxxflags: None,
+        ldflags: None,
+        keep: false,
+        generator,
+        run_args,
+        build_args: vec![],
+    };
+
+    build.build()?;
+    build.run()
+}*/
+
+pub struct Source {
+    pub halide_path: PathBuf,
+    pub repo: String,
+    pub branch: String,
+    pub make: String,
+    pub make_flags: Vec<String>,
+}
+
+impl Source {
+    pub fn download(&self) -> io::Result<bool> {
+        Command::new("git")
+            .arg("clone")
+            .args(&["-b", self.branch.as_str()])
+            .arg(&self.repo)
+            .arg(&self.halide_path)
+            .status()
+            .map(|status| status.success())
+    }
+
+    pub fn update(&self) -> io::Result<bool> {
+        Command::new("git")
+            .current_dir(&self.halide_path)
+            .arg("pull")
+            .arg("origin")
+            .arg(&self.branch)
+            .status()
+            .map(|status| status.success())
+    }
+
+    pub fn build(&self) -> io::Result<bool> {
+        Command::new(&self.make)
+            .current_dir(&self.halide_path)
+            .args(&self.make_flags)
+            .status()
+            .map(|status| status.success())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,319 @@
+mod compile;
+
+use self::compile::*;
+
+use clap::{App, Arg, SubCommand};
+
+use std::env;
+use std::path::{Path, PathBuf};
+use std::process::exit;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+static mut QUIET: bool = false;
+
+macro_rules! log {
+    ($fmt:expr, $($arg:tt)*) => {
+        unsafe {
+            if !QUIET {
+                eprintln!($fmt, $($arg)*);
+            }
+        }
+    };
+
+    ($fmt:expr) => {
+        unsafe {
+            if !QUIET {
+                eprintln!($fmt);
+            }
+        }
+    }
+}
+
+fn relative_to_home<P: AsRef<Path>>(path: P) -> PathBuf {
+    let home = PathBuf::from(env::var("HOME").expect("Cannot find HOME directory"));
+    home.join(path.as_ref())
+}
+
+fn src_command<'a, 'b>() -> App<'a, 'b> {
+    SubCommand::with_name("src")
+        .about("Download, build and update halide source")
+        .arg(
+            Arg::with_name("make")
+                .short("m")
+                .long("make")
+                .default_value("make")
+                .help("Make executable"),
+        )
+        .arg(
+            Arg::with_name("source")
+                .long("url")
+                .default_value("https://github.com/halide/halide")
+                .help("Halide respository"),
+        )
+        .arg(
+            Arg::with_name("branch")
+                .long("branch")
+                .default_value("master")
+                .help("Halide source branch"),
+        )
+}
+
+fn build_command<'a, 'b>() -> App<'a, 'b> {
+    SubCommand::with_name("build")
+        .about("Build Halide source files")
+        .arg(
+            Arg::with_name("cxx")
+                .long("cxx")
+                .env("CXX")
+                .default_value("c++")
+                .help("Set c++ compiler"),
+        )
+        .arg(
+            Arg::with_name("cxxflags")
+                .long("cxxflags")
+                .env("CXXFLAGS")
+                .help("Set c++ compile flags"),
+        )
+        .arg(
+            Arg::with_name("ldflags")
+                .long("ldflags")
+                .env("LDFLAGS")
+                .help("Set c++ link flags"),
+        )
+        .arg(
+            Arg::with_name("name")
+                .required(true)
+                .help("Output executable name"),
+        )
+        .arg(
+            Arg::with_name("input")
+                .multiple(true)
+                .required(true)
+                .help("Input files"),
+        )
+        .arg(
+            Arg::with_name("args")
+                .multiple(true)
+                .raw(true)
+                .takes_value(true)
+                .help("Arguments to executable"),
+        )
+        .arg(
+            Arg::with_name("generator")
+                .long("generator")
+                .short("g")
+                .help("Link with GenGen.cpp"),
+        )
+}
+
+fn run_command<'a, 'b>() -> App<'a, 'b> {
+    SubCommand::with_name("run")
+        .about("Build and run Halide source files")
+        .arg(
+            Arg::with_name("cxx")
+                .long("cxx")
+                .env("CXX")
+                .default_value("c++")
+                .help("Set c++ compiler"),
+        )
+        .arg(
+            Arg::with_name("cxxflags")
+                .long("cxxflags")
+                .env("CXXFLAGS")
+                .help("Set c++ compile flags"),
+        )
+        .arg(
+            Arg::with_name("ldflags")
+                .long("ldflags")
+                .env("LDFLAGS")
+                .help("Set c++ link flags"),
+        )
+        .arg(
+            Arg::with_name("keep")
+                .long("keep")
+                .short("k")
+                .help("Keep generated executables"),
+        )
+        .arg(
+            Arg::with_name("generator")
+                .long("generator")
+                .short("g")
+                .help("Link with GenGen.cpp"),
+        )
+        .arg(
+            Arg::with_name("input")
+                .multiple(true)
+                .required(true)
+                .help("Input files"),
+        )
+        .arg(
+            Arg::with_name("args")
+                .multiple(true)
+                .raw(true)
+                .takes_value(true)
+                .help("Arguments to executable"),
+        )
+}
+
+fn main() {
+    let default_halide_path = relative_to_home("halide");
+    let mut app = App::new("halide")
+        .version("0.1")
+        .author("Zach Shipko <zachshipko@gmail.com>")
+        .arg(
+            Arg::with_name("quiet")
+                .short("q")
+                .help("Disable logging to stdout/stderr"),
+        )
+        .arg(
+            Arg::with_name("halide-path")
+                .short("p")
+                .env("HALIDE_PATH")
+                .default_value(default_halide_path.to_str().expect("Invalid path"))
+                .help("Path to Halide directory"),
+        )
+        .subcommand(src_command())
+        .subcommand(build_command())
+        .subcommand(run_command());
+
+    let mut help = Vec::new();
+    let _ = app.write_long_help(&mut help);
+    let matches = app.get_matches();
+
+    unsafe {
+        QUIET = matches.is_present("quiet");
+    }
+
+    let halide_path = Path::new(
+        matches
+            .value_of("halide-path")
+            .expect("Invalid HALIDE_PATH"),
+    );
+
+    if let Some(src) = matches.subcommand_matches("src") {
+        let source = Source {
+            halide_path: halide_path.to_owned(),
+            repo: src
+                .value_of("source")
+                .expect("Invalid source repository")
+                .to_string(),
+            branch: src
+                .value_of("branch")
+                .expect("Invalid branch name")
+                .to_string(),
+            make: src
+                .value_of("make")
+                .expect("Invalid make executable")
+                .to_string(),
+            make_flags: src
+                .values_of("make-flags")
+                .unwrap_or(clap::Values::default())
+                .map(|s| s.to_string())
+                .collect(),
+        };
+
+        if halide_path.exists() {
+            log!(
+                "Updating Halide source in {}",
+                halide_path.to_string_lossy()
+            );
+            if !source.update().expect("Error updating git repository") {
+                log!("Failed to update git repository");
+                exit(1)
+            }
+        } else {
+            log!(
+                "Downloading Halide source to {}",
+                halide_path.to_string_lossy()
+            );
+            if !source.download().expect("Error downloading git repository") {
+                log!("Failed to clone git repository");
+                exit(1)
+            }
+        }
+
+        if !source.build().expect("Error building Halide source") {
+            log!("Halide build failed");
+            exit(1)
+        } else {
+            log!(
+                "Halide built successfully in {}",
+                halide_path.to_string_lossy()
+            );
+        }
+    } else if let Some(b) = matches.subcommand_matches("build") {
+        let build = Build {
+            halide_path: halide_path.to_owned(),
+            cxx: b.value_of("cxx"),
+            cxxflags: b.value_of("cxxflags"),
+            ldflags: b.value_of("ldflags"),
+            output: PathBuf::from(b.value_of("name").expect("Invalid output path")),
+            src: b
+                .values_of("input")
+                .expect("Invalid input files")
+                .map(|x| PathBuf::from(x))
+                .collect(),
+            keep: true,
+            build_args: b
+                .values_of("args")
+                .unwrap_or(clap::Values::default())
+                .collect(),
+            run_args: vec![],
+            generator: b.is_present("generator"),
+        };
+
+        log!("Compiling {:?} to {:?}", build.src, build.output);
+        if !build
+            .build()
+            .expect(format!("Error building {:?}", build.output).as_str())
+        {
+            log!("Unable to build {:?}", build.output);
+            exit(1)
+        }
+    } else if let Some(b) = matches.subcommand_matches("run") {
+        let start = SystemTime::now();
+        let ts = start.duration_since(UNIX_EPOCH).unwrap();
+        let ms = ts.as_secs() * 1000 + ts.subsec_nanos() as u64 / 1000000;
+        let build = Build {
+            halide_path: halide_path.to_owned(),
+            cxx: b.value_of("cxx"),
+            cxxflags: b.value_of("cxxflags"),
+            ldflags: b.value_of("ldflags"),
+            output: PathBuf::from(format!("./halide-{}", ms)),
+            src: b
+                .values_of("input")
+                .expect("Invalid input files")
+                .map(|x| PathBuf::from(x))
+                .collect(),
+            keep: b.is_present("keep"),
+            run_args: b
+                .values_of("args")
+                .unwrap_or(clap::Values::default())
+                .collect(),
+            build_args: vec![],
+            generator: b.is_present("generator"),
+        };
+
+        let output = build.output.to_owned();
+
+        log!("Compiling {:?} to {:?}", build.src, output);
+        if !build
+            .build()
+            .expect(format!("Error building {:?}", build.src).as_str())
+        {
+            log!("Failure building {:?}", build.src);
+            exit(1)
+        }
+
+        log!("Running {:?}", build.output);
+        if !build
+            .run()
+            .expect(format!("Error running {:?}", build.output).as_str())
+        {
+            log!("Failure while running {:?}", build.output);
+            exit(1)
+        }
+    } else {
+        eprintln!("{}", String::from_utf8_lossy(help.as_ref()));
+    }
+}
